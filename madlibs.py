3a73e55877177c49c231569457b590e3adeedd20
spitum <53660198+spitum@users.noreply.github.com>
2019-08-17 11:37:23 -0600

    Add files via upload
    prints out contents from a text file and then creates a new file with words replaced that are in a dictionary. 
    Automate the boring stuff - chapter 8

---
+++ madlibs.py
@@ -0,0 +1,60 @@
+# Create a Mad Libs program that reads in text files and lets the user add their own text 
+# anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file. For example, a text file may look like this:
+
+# The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was
+# unaffected by these events.
+# The program would find these occurrences and prompt the user to replace them.
+
+
+# Enter an adjective:
+# silly
+# Enter a noun:
+# chandelier
+# Enter a verb:
+# screamed
+# Enter a noun:
+# pickup truck
+# The following text file would then be created:
+
+
+# The silly panda walked to the chandelier and then screamed. A nearby pickup
+# truck was unaffected by these events.
+# The results should be printed to the screen and saved to a new text file.
+
+
+madlibs = 'madlibs.txt'
+
+with open(madlibs,'r') as f:
+    contents = f.read()
+    print(contents)
+
+def input_words():
+    words = {'adj': '', 'verb': '', 'noun': ''}
+    adj = input('Please enter adjective: ')
+    if adj != None:
+        words['adj'] = adj
+    verb = input('Please enter verb: ')
+    if verb != None:
+        words['verb'] = verb
+    noun = input('Please enter noun: ')
+    if noun != None:
+        words['noun'] = noun
+    with open('madlibs_%s.txt' % (words['noun']),'w') as f:
+        f.write(f'The %s panda walked to the %s and then %s. A nearby %s was unaffected by these events.' % (words['adj'], words['noun'],words['verb'], words['noun']))
+    print(f'The %s panda walked to the %s and then %s. A nearby %s was unaffected by these events.' % (words['adj'], words['noun'],words['verb'], words['noun']) )
+
+#print(input_words())
+
+if __name__ == '__main__':
+    input_words()
+
+
+
+
+
+
+
+
+
+
+
