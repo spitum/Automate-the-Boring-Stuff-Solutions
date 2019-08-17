a0be2de7b1300d1b74f080f14a6270a4881729ef
spitum <53660198+spitum@users.noreply.github.com>
2019-08-17 14:31:53 -0600

    searches all files within a folder for regex
    ATBS - chapter 8 - project 2. 
    
    #TODO:
    change to function
    add if __name__ == '__main__'

---
+++ regex_read_allfiles.py
@@ -0,0 +1,28 @@
+#Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. The results should be printed to the screen.
+
+import glob
+import os
+import re
+
+#os.chdir('C:/Users/spitu/Documents/')
+#print(os.getcwd())
+path = os.getcwd()
+fileList = glob.glob("*.txt")
+
+# below section is from: https://stackoverflow.com/questions/51691270/python-user-input-as-regular-expression-how-to-do-it-correctly
+# helps validate regex input before checking files
+user_input = input("Input regex:")  # check console, it is expecting your input
+regex_string = r"%s" % user_input
+string_to_search = re.compile(regex_string)
+print("User typed: '{}'. Input type: {}.Regex: {}".format(user_input, type(user_input), string_to_search))
+
+
+for file in fileList:
+    with open(file,'r') as file_object:
+        contents = file_object.readlines() 
+        #print(contents)
+        for line in contents:
+                word_search = string_to_search.finditer(line)
+                for word in word_search:
+                        print(file_object,word)
+
