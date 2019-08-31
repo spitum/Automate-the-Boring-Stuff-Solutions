f018a321f92e96f47972da7f054d4b721d9f2f05
spitum <53660198+spitum@users.noreply.github.com>
2019-08-31 13:33:34 -0600

    Update BlankRowInserter.py
    Creates a new excel file with user input filename. Adds blank rows after row specified row.

--- chapter 12/BlankRowInserter.py
+++ chapter 12/BlankRowInserter.py
@@ -1,1 +1,39 @@
 
+#! blankRowInserter.py
+# You can write this program by reading in the contents of the spreadsheet.
+# Then, when writing out the new spreadsheet, use a for loop to copy the first N lines. 
+# For the remaining lines, add M to the row number in the output spreadsheet.
+
+import openpyxl
+import sys
+import os
+
+N  = int(sys.argv[1])
+M = int(sys.argv[2])
+Filename = input('Please enter the new file name (xlsx).')
+
+print('Opening workbook...')
+
+wb = openpyxl.Workbook()
+dest_filename = Filename + '.xlsx'
+
+ws1 = wb.active
+ws1.title = "Updated"
+
+sourcefilename = os.path.basename(sys.argv[3])
+
+example = openpyxl.load_workbook(sourcefilename)
+
+sheet = example.active
+
+print('Writing to workbook: %s' %(dest_filename))
+
+for i in range(1, sheet.max_row):
+    for j in range(1, sheet.max_column+1):
+        ws1.cell(row=i,column=j).value = sheet.cell(row=i,column=j).value
+
+ws1.insert_rows(N+1,M) #insert after the row specified.
+print('Number of rows written: %s' %(ws1.max_row))
+wb.save(filename = dest_filename)
+
+print('Done')
